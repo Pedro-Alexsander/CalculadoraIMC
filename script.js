a611c57e0e1f9a701f9b336eca3afca9106ca5f9
Pedro Alexsander <pedroalexsander22@gmail.com>
2022-03-01 11:26:33 -0300

    Lógica da calculadora

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function imc() {
+function calc() {
   let altura = parseFloat(document.getElementById("altura").value);
   let peso = parseFloat(document.getElementById("peso").value);
-  console.log(peso / (altura * altura));
+  if (altura < 9) {
+    // foi a maneira que encotrei de burlar caso a pessoa coloque um número quebrado. Ex 1.83
+    altura = altura * 100;
+  }
+  altura = altura / 100;
+
+  let imc = peso / (altura * altura);
+  console.log(imc.toFixed(2));
+  console.log(altura);
 }
