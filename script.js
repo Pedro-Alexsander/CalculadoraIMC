4761ecfd98918202d5ed23ab0b7fe330cc7f4396
Pedro Alexsander <pedroalexsander22@gmail.com>
2022-03-02 15:58:27 -0300

    prevenindo possíveis erros com o 0

--- script.js
+++ script.js
@@ -10,38 +10,58 @@ function calc() {
   let imc = peso / (altura * altura);
   imc = parseFloat(imc.toFixed(2));
   let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-  
-
-
   console.log(imc)
-  if (imc <= 0) {
-    console.log("logou")
+  
+  
+  
+  if(altura == 0 && peso == 0){
     mensagem = (document.getElementById("mensagem").innerHTML = 0)
     let error = (document.getElementById("error").innerHTML =
-    "Informe um valor positivo e diferente de zero");
+    "Informe um valor diferente de zero");
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+    " ");
+  }if(altura == 0 && peso !== 0){
+    mensagem = (document.getElementById("mensagem").innerHTML = 0)
+    error = (document.getElementById("error").innerHTML =
+    "Informe sua altura");
+    classficacao = (document.getElementById("classificacao").innerHTML =
+    " ");
+  }if(peso == 0 && altura !== 0){
+    mensagem = (document.getElementById("mensagem").innerHTML = 0)
+    error = (document.getElementById("error").innerHTML =
+    "Informe seu peso");
+    classficacao = (document.getElementById("classificacao").innerHTML =
+    " ");
+  }
+  if (altura < 0 || peso < 0 ) {
+    mensagem = (document.getElementById("mensagem").innerHTML = 0)
+    error = (document.getElementById("error").innerHTML =
+    "Informe um valor positivo");
+    classficacao = (document.getElementById("classificacao").innerHTML =
+    " ");
   }
   if (imc < 18.5 && imc>0) {
     let classficacao = (document.getElementById("classificacao").innerHTML =
       "Abaixo do peso");
   }
   if (imc >= 18.5 && imc < 25) {
-    let classficacao = (document.getElementById("classificacao").innerHTML =
+     classficacao = (document.getElementById("classificacao").innerHTML =
       "Peso normal");
   }
   if (imc >= 25 && imc < 30) {
-    let classficacao = (document.getElementById("classificacao").innerHTML =
+     classficacao = (document.getElementById("classificacao").innerHTML =
       "Sobrepeso");
   }
   if (imc >= 30 && imc < 35) {
-    let classficacao = (document.getElementById("classificacao").innerHTML =
+     classficacao = (document.getElementById("classificacao").innerHTML =
       "Obsidade grau I");
   }
   if (imc >= 35 && imc < 40) {
-    let classficacao = (document.getElementById("classificacao").innerHTML =
+    classficacao = (document.getElementById("classificacao").innerHTML =
       "Obsidade grau II");
   }
-  if (imc >= 40) {
-    let classficacao = (document.getElementById("classificacao").innerHTML =
+  if (imc >= 40 && peso !== 0 && altura !== 0) {
+     classficacao = (document.getElementById("classificacao").innerHTML =
       "Obsidade Mórbida");
   }
 }
