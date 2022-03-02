67f2aa83e2995c578121f604598fcdda95adbae9
Pedro Alexsander <pedroalexsander22@gmail.com>
2022-03-02 15:32:44 -0300

    prevenindo erro com número negativo

--- script.js
+++ script.js
@@ -8,25 +8,40 @@ function calc() {
   altura = altura / 100;
 
   let imc = peso / (altura * altura);
-  imc = imc.toFixed(2);
+  imc = parseFloat(imc.toFixed(2));
+  let mensagem = (document.getElementById("mensagem").innerHTML = imc);
+  
 
-  if (imc < 18.5) {
-    let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-    let classficacao = (document.getElementById("classificacao").innerHTML = "Abaixo do peso")
-  }if(imc >= 18.5 && imc<25 ){
-    let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-    let classficacao = (document.getElementById("classificacao").innerHTML = "Peso normal")
-  }if(imc >= 25 && imc<30 ){
-    let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-    let classficacao = (document.getElementById("classificacao").innerHTML = "Sobrepeso")
-  }if(imc >= 30 && imc<35 ){
-    let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-    let classficacao = (document.getElementById("classificacao").innerHTML = "Obsidade grau I")
-  }if(imc >= 35 && imc<40 ){
-    let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-    let classficacao = (document.getElementById("classificacao").innerHTML = "Obsidade grau II")
-  }if(imc >= 40 ){
-    let mensagem = (document.getElementById("mensagem").innerHTML = imc);
-    let classficacao = (document.getElementById("classificacao").innerHTML = "Obsidade Mórbida")
+
+  console.log(imc)
+  if (imc <= 0) {
+    console.log("logou")
+    mensagem = (document.getElementById("mensagem").innerHTML = 0)
+    let error = (document.getElementById("error").innerHTML =
+    "Informe um valor positivo e diferente de zero");
+  }
+  if (imc < 18.5 && imc>0) {
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+      "Abaixo do peso");
+  }
+  if (imc >= 18.5 && imc < 25) {
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+      "Peso normal");
+  }
+  if (imc >= 25 && imc < 30) {
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+      "Sobrepeso");
+  }
+  if (imc >= 30 && imc < 35) {
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+      "Obsidade grau I");
+  }
+  if (imc >= 35 && imc < 40) {
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+      "Obsidade grau II");
+  }
+  if (imc >= 40) {
+    let classficacao = (document.getElementById("classificacao").innerHTML =
+      "Obsidade Mórbida");
   }
 }
